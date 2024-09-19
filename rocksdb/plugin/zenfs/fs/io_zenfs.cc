447f2ba4489ed6c389ef108872921238c2a7c7ad
minsky922 <minsky922@gmail.com>
2024-09-19 17:19:21 +0900

    debug

--- rocksdb/plugin/zenfs/fs/io_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/io_zenfs.cc
@@ -1170,12 +1170,12 @@ IOStatus ZonedWritableFile::PositionedAppend(const Slice& data, uint64_t offset,
 }
 
 void ZonedWritableFile::SetWriteLifeTimeHint(Env::WriteLifeTimeHint hint) {
-  if (zoneFile_->is_sst_) {
-    zoneFile_->fno_ = fno_;
-    zoneFile_->input_fno_ = input_fno_;
-    // zoneFile_->GetZbd()->SetSSTFileforZBDNoLock(fno_, zoneFile_.get());
-    zoneFile_->level_ = level_;
-  }
+  // if (zoneFile_->is_sst_) {
+  zoneFile_->fno_ = fno_;
+  zoneFile_->input_fno_ = input_fno_;
+  // zoneFile_->GetZbd()->SetSSTFileforZBDNoLock(fno_, zoneFile_.get());
+  zoneFile_->level_ = level_;
+  // }
   zoneFile_->SetWriteLifeTimeHint(hint, level_);
 }
 
