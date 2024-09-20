963cdd745ab07b94fcfdf56005bfb292842e9ed0
minsky922 <minsky922@gmail.com>
2024-09-20 15:19:34 +0900

    compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 ROCKSDB_PATH=$HOME/EZC/rocksdb
 ZENFS_PATH=$HOME/EZC/rocksdb/plugin/zenfs
 
-cd ${ZENFS_PATH} && git pull
+# cd ${ZENFS_PATH} && git pull
+git pull https://minsky922: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@github.com/minsky922/EZC
 
 cd ${ROCKSDB_PATH} && sudo DEBUG_LEVEL=0 ROCKSDB_PLUGINS=zenfs make -j16 db_bench install
 
