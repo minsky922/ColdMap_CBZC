b3845b75174f407932f48f3c3ba85e4fca86b609
minsky922 <minsky922@gmail.com>
2024-10-18 21:05:44 +0900

    debug

--- rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
@@ -726,7 +726,7 @@ inline uint64_t ZonedBlockDevice::LazyExponential(uint64_t sz, uint64_t fr,
 void ZonedBlockDevice::CalculateResetThreshold(uint64_t free_percent) {
   uint64_t rt = 0;
   uint64_t max_capacity = io_zones[0]->max_capacity_;
-  uint64_t free_percent = cur_free_percent_;
+  // uint64_t free_percent = cur_free_percent_;
   switch (reset_scheme_) {
     case kEager:
       rt = max_capacity;
