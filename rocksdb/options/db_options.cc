226e7a48999e38dc9833ff56f406ae8278dfe8ae
minsky922 <minsky922@gmail.com>
2024-10-18 19:50:12 +0900

    debug

--- rocksdb/options/db_options.cc
+++ rocksdb/options/db_options.cc
@@ -838,12 +838,6 @@ ImmutableDBOptions::ImmutableDBOptions(const DBOptions& options)
   clock = env->GetSystemClock().get();
   logger = info_log.get();
   stats = statistics.get();
-  if (row_cache == nullptr && row_cache_size) {
-    // printf("New Row cache size %lu\n",row_cache_size);
-    row_cache = NewLRUCache(row_cache_size);
-  } else {
-    // printf("row cache %p , %lu\n",row_cache.get(),row_cache_size);
-  }
 }
 
 void ImmutableDBOptions::Dump(Logger* log) const {
