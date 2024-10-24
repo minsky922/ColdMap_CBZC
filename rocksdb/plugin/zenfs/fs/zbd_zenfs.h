9c920ee82f57122a35056c4436a49fb319c10fa2
minsky922 <minsky922@gmail.com>
2024-10-24 23:27:50 +0900

    d

--- rocksdb/plugin/zenfs/fs/zbd_zenfs.h
+++ rocksdb/plugin/zenfs/fs/zbd_zenfs.h
@@ -522,7 +522,13 @@ class ZonedBlockDevice {
     cumulative_io_blocking_ += (ns / 1000) / 1000;
   }
   void AddCalculatelifetimeLapse(long ns) {
-    calculate_lapse += (ns / 1000) / 1000;
+    if (zc_scheme_ == CBZC3) {
+      // printf("calculate lifetime CBZC3!!\n");
+      calculate_lapse += (ns / 1000) / 1000;
+    } else {
+      printf("calculate lifetime CBZC1,2!!");
+      calculate_lapse += (ns / 1000) / 1000;
+    }
   }
 
   uint64_t CalculateCapacityRemain() {
