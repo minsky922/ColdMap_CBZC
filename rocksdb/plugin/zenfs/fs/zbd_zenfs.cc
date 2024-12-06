e981789ce7caded348e66f2ec76421fc5ce41253
gs201 <sg20180546@gmail.com>
2024-12-06 19:29:16 +0900

    openzone8

--- rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
@@ -269,28 +269,30 @@ IOStatus ZonedBlockDevice::Open(bool readonly, bool exclusive) {
                                   " required)");
   }
 
-  // if (max_nr_active_zones == 0)
-  //   max_nr_active_io_zones_ = zbd_be_->GetNrZones();
-  // else
-  //   max_nr_active_io_zones_ = max_nr_active_zones - reserved_zones;
+  if (max_nr_active_zones == 0)
+    max_nr_active_io_zones_ = zbd_be_->GetNrZones();
+  else
+    max_nr_active_io_zones_ = max_nr_active_zones - reserved_zones;
 
-  // if (max_nr_open_zones == 0)
-  //   max_nr_open_io_zones_ = zbd_be_->GetNrZones();
-  // else
-  //   max_nr_open_io_zones_ = max_nr_open_zones - reserved_zones;
+  if (max_nr_open_zones == 0)
+    max_nr_open_io_zones_ = zbd_be_->GetNrZones();
+  else
+    max_nr_open_io_zones_ = max_nr_open_zones - reserved_zones;
 
-  if (max_nr_active_zones == 0) {
-    max_nr_active_zones = 13;
-    max_nr_active_io_zones_ = 14 - 1;
-  } else {
-    max_nr_active_io_zones_ = 14 - 1;
-  }
-  if (max_nr_open_zones == 0) {
-    max_nr_open_zones = 13;
-    max_nr_open_io_zones_ = 14 - 1;
-  } else {
-    max_nr_open_io_zones_ = 14 - 1;
-  }
+  max_nr_active_io_zones_=8;
+  max_nr_open_io_zones_=8;
+  // if (max_nr_active_zones == 0) {
+  //   max_nr_active_zones = 13;
+  //   max_nr_active_io_zones_ = 14 - 1;
+  // } else {
+  //   max_nr_active_io_zones_ = 14 - 1;
+  // }
+  // if (max_nr_open_zones == 0) {
+  //   max_nr_open_zones = 13;
+  //   max_nr_open_io_zones_ = 14 - 1;
+  // } else {
+  //   max_nr_open_io_zones_ = 14 - 1;
+  // }
 
   Info(logger_, "Zone block device nr zones: %u max active: %u max open: %u \n",
        zbd_be_->GetNrZones(), max_nr_active_zones, max_nr_open_zones);
