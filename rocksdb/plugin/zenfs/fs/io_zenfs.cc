4ff5fceb0e55f2b6f3f9ffe4a0c6427c082f52ec
gs201 <sg20180546@gmail.com>
2025-06-08 18:52:43 +0900

    fegcanother

--- rocksdb/plugin/zenfs/fs/io_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/io_zenfs.cc
@@ -38,6 +38,8 @@ namespace ROCKSDB_NAMESPACE {
 
 ZoneExtent::ZoneExtent(uint64_t start, uint64_t length, Zone* zone)
     : start_(start), length_(length), zone_(zone), is_zc_copied_(false),ZC_COPIED_STATE(NO_COPIED) {
+              auto now = std::chrono::system_clock::now();
+
       create_time_= std::chrono::duration_cast<std::chrono::milliseconds>(
                                     now.time_since_epoch()).count();
     }
@@ -55,6 +57,8 @@ ZoneExtent::ZoneExtent(uint64_t start, uint64_t length, Zone* zone,
       ZC_COPIED_STATE(NO_COPIED) {
   zc_copied_ts_.tv_sec = 0;
   zc_copied_ts_.tv_nsec = 0;
+          auto now = std::chrono::system_clock::now();
+
   create_time_= std::chrono::duration_cast<std::chrono::milliseconds>(
                                     now.time_since_epoch()).count();
   if (zone == nullptr) {
